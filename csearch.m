b0d79be1ec977d43e9956c7c1d9cf9a354895593
Peter Cook <30669173+peterfranciscook@users.noreply.github.com>
2019-07-29 11:45:03 -0600

    Add m-help-file

---
+++ csearch.m
@@ -0,0 +1,25 @@
+%CSEARCH Fast Lookup and Masking for Sorted Arrays
+%
+%   argmax = csearch(x, 'lt', y) is equivalent to MATLAB's find(x<y, 1, 'last')
+%
+%   argmax = csearch(x, 'le', y) is equivalent to MATLAB's find(x<=y, 1, 'last')
+%
+%   argmin = csearch(x, 'gt', y) is equivalent to MATLAB's find(x>y, 1, 'last')
+%
+%   argmin = csearch(x, 'ge', y) is equivalent to MATLAB's find(x>=y, 1, 'last')
+%
+%   Class Support
+%   -------------
+%   The input array x must be a 1D finite, real, non-sparse array of
+%   the following classes: uint8, int8, uint16, int16, uint32, int32,
+%   uint64, int64, single or double. 
+%
+%
+%   Notes
+%   -----
+%   1. If input y is outside the range of the array x, -1 is returned
+%
+%   2. Input y should be the same class as x
+%
+%
+% Peter Cook 2019
